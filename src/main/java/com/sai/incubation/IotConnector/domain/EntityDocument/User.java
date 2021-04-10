8b1cb66b9ef81fb748667405bab5d7f3dacbd51b
Nishanth Murugan <nishanth.murugan@gmail.com>
2021-04-10 14:14:46 +0530

    User controller object

---
+++ src/main/java/com/sai/incubation/IotConnector/domain/EntityDocument/User.java
@@ -0,0 +1,155 @@
+package com.sai.incubation.IotConnector.domain.EntityDocument;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+
+	@Id
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String areaCode;
+	private String mobileNumber;
+	private String dob;
+	private String gender;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	private String creator;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAreaCode() {
+		return areaCode;
+	}
+
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+}
---
+++ src/main/java/com/sai/incubation/IotConnector/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sai.incubation.IotConnector.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.sai.incubation.IotConnector.domain.EntityDocument.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+	User findByEmail(String email);
+
+}
